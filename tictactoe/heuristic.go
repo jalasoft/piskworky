b006068efe77c0611e75a722f4daef8a9b4d3d66
Lastovicka <j.lastovicka@gmail.com>
2022-05-28 22:47:22 +0200

    heuristic enhanced

--- tictactoe/heuristic.go
+++ tictactoe/heuristic.go
@@ -4,99 +4,144 @@ import (
 	"fmt"
 )
 
-func EvaluateAIPositions(game gameState) {
-	game.aiPositions.ForEach(func(p Position) {
-		evaluatePosition(p, game.aiPositions, game.opponentPositions, game.area)
-	})
-}
-
-func evaluatePosition(p Position, myPos PositionSet, opponentPos PositionSet, area Area) {
-	discovery := discover(p, Left, p, Right, p, 0, myPos, opponentPos, area, make(map[segment]discovery))
-
-	if discovery.status == 1 {
-		fmt.Println("Nenasel jsem potencialni vyhru")
-	} else {
-		fmt.Printf("counts: %v\n", discovery.myPosCounts)
-	}
-}
-
-type discovery struct {
-	status      uint8
-	myPosCounts []uint8
-}
-
 type segment struct {
 	p1 Position
 	p2 Position
 }
 
-func discover(p1 Position, d1 PositionDirection, p2 Position, d2 PositionDirection, newPos Position, distance uint8, myPos PositionSet, opponentPos PositionSet, area Area, memo map[segment]discovery) discovery {
+func (s *segment) String() string {
+	return fmt.Sprintf("%v-%v", s.p1, s.p2)
+}
 
-	if !p1.isWithinBounds(area) || !p2.isWithinBounds(area) {
-		return discovery{1, []uint8{}}
+type discovery struct {
+	status   uint8
+	segments map[segment]uint8
+}
+
+func (d *discovery) merge(another *discovery) discovery {
+	newSegments := make(map[segment]uint8)
+
+	for segment, count := range d.segments {
+		newSegments[segment] = count
 	}
 
-	if opponentPos.Has(p1) || opponentPos.Has(p2) {
-		return discovery{1, []uint8{}}
+	for segment, count := range another.segments {
+		if _, has := newSegments[segment]; !has {
+			newSegments[segment] = count
+		}
+	}
+	return discovery{0, newSegments}
+}
+
+func (d *discovery) increment() discovery {
+	newSegments := make(map[segment]uint8)
+
+	for segment, count := range d.segments {
+		newSegments[segment] = count + 1
+	}
+	return discovery{0, newSegments}
+}
+
+func (d discovery) String() string {
+	str := "discovery{\n"
+	for s, c := range d.segments {
+		str = fmt.Sprintf("%s\t%v:%d\n", str, s, c)
+	}
+	str = str + "}\n"
+	return str
+}
+
+//----------------------------------------------------------------
+
+func EvaluateAIPositions(game gameState) discovery {
+	all := discovery{0, make(map[segment]uint8)}
+	memo := make(map[segment]discovery)
+
+	game.aiPositions.ForEach(func(p Position) {
+		discovery := evaluatePosition(p, game.aiPositions, game.opponentPositions, game.area, memo)
+		all = all.merge(&discovery)
+	})
+
+	return all
+}
+
+func evaluatePosition(p Position, myPos PositionSet, opponentPos PositionSet, area Area, memo map[segment]discovery) discovery {
+	d := discovery{}
+
+	lr := discoverSegments(p, Left, Right, 0, myPos, opponentPos, area, memo)
+	d = d.merge(&lr)
+
+	tb := discoverSegments(p, Top, Bottom, 0, myPos, opponentPos, area, memo)
+	d = d.merge(&tb)
+
+	tlbr := discoverSegments(p, TopLeft, BottomRight, 0, myPos, opponentPos, area, memo)
+	d = d.merge(&tlbr)
+
+	trbl := discoverSegments(p, TopRight, BottomLeft, 0, myPos, opponentPos, area, memo)
+	d = d.merge(&trbl)
+
+	return d
+}
+
+func discoverSegments(p Position, d1 PositionDirection, d2 PositionDirection, distance uint8, myPos PositionSet, opponentPos PositionSet, area Area, memo map[segment]discovery) discovery {
+	discovery := discover(segment{p, p}, d1, d2, distance, myPos, opponentPos, area, memo)
+
+	if discovery.status == 0 && myPos.Has(p) {
+		discovery = discovery.increment()
+	}
+
+	return discovery
+}
+
+func discover(s segment, d1 PositionDirection, d2 PositionDirection, distance uint8, myPos PositionSet, opponentPos PositionSet, area Area, memo map[segment]discovery) discovery {
+
+	if !s.p1.isWithinBounds(area) || !s.p2.isWithinBounds(area) {
+		return discovery{1, nil}
+	}
+
+	if opponentPos.Has(s.p1) || opponentPos.Has(s.p2) {
+		return discovery{1, nil}
 	}
 
 	if distance == 4 {
-		if myPos.Has(newPos) {
-			return discovery{0, []uint8{1}}
-		}
-		return discovery{0, []uint8{0}}
+		return discovery{0, map[segment]uint8{s: 0}}
 	}
 
-	newP1 := p1.next(d1)
-	newSegment1 := segment{newP1, p2}
+	newP1 := s.p1.next(d1)
+	newSegment1 := segment{newP1, s.p2}
 
 	discovery1, has1 := memo[newSegment1]
 
 	if !has1 {
-		memo[newSegment1] = discover(newP1, d1, p2, d2, newP1, distance+1, myPos, opponentPos, area, memo)
+		memo[newSegment1] = discover(newSegment1, d1, d2, distance+1, myPos, opponentPos, area, memo)
 		discovery1 = memo[newSegment1]
-
-		if discovery1.status == 0 && myPos.Has(newPos) {
-			incrementDiscovery(&discovery1)
-		}
 	}
 
-	newP2 := p2.next(d2)
-	newSegment2 := segment{p1, newP2}
+	if discovery1.status == 0 && myPos.Has(newP1) {
+		discovery1 = discovery1.increment()
+	}
+
+	newP2 := s.p2.next(d2)
+	newSegment2 := segment{s.p1, newP2}
 
 	discovery2, has2 := memo[newSegment2]
 
 	if !has2 {
-		memo[newSegment2] = discover(p1, d1, newP2, d2, newP2, distance+1, myPos, opponentPos, area, memo)
+		memo[newSegment2] = discover(newSegment2, d1, d2, distance+1, myPos, opponentPos, area, memo)
 		discovery2 = memo[newSegment2]
+	}
 
-		if discovery2.status == 0 && myPos.Has(newPos) {
-			incrementDiscovery(&discovery2)
-		}
+	if discovery2.status == 0 && myPos.Has(newP2) {
+		discovery2 = discovery2.increment()
 	}
 
 	if discovery1.status == 0 && discovery2.status == 0 {
-		return *mergeDiscovery(&discovery1, &discovery2)
+		return discovery1.merge(&discovery2)
 	} else if discovery1.status == 0 {
 		return discovery1
 	} else if discovery2.status == 0 {
 		return discovery2
 	} else {
-		return discovery{1, []uint8{}}
+		return discovery{1, nil}
 	}
-}
-
-func incrementDiscovery(d *discovery) {
-	newIncrements := make([]uint8, 0, len(d.myPosCounts))
-
-	for _, c := range d.myPosCounts {
-		newIncrements = append(newIncrements, uint8(c+1))
-	}
-
-	d.myPosCounts = newIncrements
-}
-
-func mergeDiscovery(d1 *discovery, d2 *discovery) *discovery {
-	d1.myPosCounts = append(d1.myPosCounts, d2.myPosCounts...)
-	return d1
 }
--- tictactoe/heuristic_test.go
+++ tictactoe/heuristic_test.go
@@ -4,8 +4,47 @@ import (
 	"testing"
 )
 
-//|ox xx |
-func TestEvaluateAIPositions(t *testing.T) {
+func assertThat(d discovery, t *testing.T) *DiscoveryAssertion {
+	return &DiscoveryAssertion{
+		d: d,
+		t: t,
+	}
+}
+
+type DiscoveryAssertion struct {
+	d discovery
+	t *testing.T
+}
+
+func (d *DiscoveryAssertion) hasSegment(expectedSegment segment, expectedCount uint8) *DiscoveryAssertion {
+
+	actualCount, exists := d.d.segments[expectedSegment]
+
+	if !exists {
+		d.t.Errorf("%v was not discovered\n", expectedSegment)
+		return d
+	}
+
+	if expectedCount != actualCount {
+		d.t.Errorf("Counts of segment %v do NOT match. Expected: %d, actual: %d\n", expectedSegment, expectedCount, actualCount)
+	}
+
+	return d
+}
+
+func (d *DiscoveryAssertion) hasTotal(expected uint8) *DiscoveryAssertion {
+
+	if len(d.d.segments) != int(expected) {
+		d.t.Errorf("Discovery has unexpected number of segments. Expected: %d, actual: %d\n", expected, len(d.d.segments))
+	}
+
+	return d
+}
+
+//--------------------TESTS------------------------
+
+//|ox.xx.|
+func TestEvaluateAIPositions1(t *testing.T) {
 	myPos := newPositionSet()
 	myPos.Add(Position{0, 1})
 	myPos.Add(Position{0, 3})
@@ -21,5 +60,149 @@ func TestEvaluateAIPositions(t *testing.T) {
 		opponentPositions: opponentPos,
 	}
 
-	EvaluateAIPositions(state)
+	discovery := EvaluateAIPositions(state)
+
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{0, 5}}, 3)
+	assertThat(discovery, t).hasTotal(1)
+}
+
+//|...x.....|
+func TestEvaluateAIPositions2(t *testing.T) {
+	myPos := newPositionSet()
+	myPos.Add(Position{0, 3})
+
+	opponentPos := newPositionSet()
+	area := Area{1, 9}
+
+	state := gameState{
+		area:              area,
+		aiPositions:       myPos,
+		opponentPositions: opponentPos,
+	}
+
+	discovery := EvaluateAIPositions(state)
+
+	assertThat(discovery, t).hasSegment(segment{Position{0, 0}, Position{0, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{0, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 2}, Position{0, 6}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 3}, Position{0, 7}}, 1)
+	assertThat(discovery, t).hasTotal(4)
+}
+
+//|o.x.x.x|
+func TestEvaluateAIPositions3(t *testing.T) {
+	myPos := newPositionSet()
+	myPos.Add(Position{0, 2})
+	myPos.Add(Position{0, 4})
+	myPos.Add(Position{0, 6})
+
+	opponentPos := newPositionSet()
+	opponentPos.Add(Position{0, 0})
+	area := Area{1, 7}
+
+	state := gameState{
+		area:              area,
+		aiPositions:       myPos,
+		opponentPositions: opponentPos,
+	}
+
+	discovery := EvaluateAIPositions(state)
+
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{0, 5}}, 2)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 2}, Position{0, 6}}, 3)
+	assertThat(discovery, t).hasTotal(2)
+}
+
+//|_____x_|
+//|____x__|
+//|_______|
+//|__x____|
+//|_______|
+//|oo_____|
+//|_______|
+func TestEvaluateAIPositions4(t *testing.T) {
+	myPos := newPositionSet()
+	myPos.Add(Position{0, 5})
+	myPos.Add(Position{1, 4})
+	myPos.Add(Position{3, 2})
+
+	opponentPos := newPositionSet()
+	opponentPos.Add(Position{5, 0})
+	opponentPos.Add(Position{5, 1})
+	area := Area{7, 7}
+
+	state := gameState{
+		area:              area,
+		aiPositions:       myPos,
+		opponentPositions: opponentPos,
+	}
+
+	discovery := EvaluateAIPositions(state)
+
+	t.Logf("%v\n", discovery)
+
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{0, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 2}, Position{0, 6}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 5}, Position{4, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 0}, Position{1, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 1}, Position{1, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 2}, Position{1, 6}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 4}, Position{4, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 4}, Position{5, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{3, 0}, Position{3, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{3, 1}, Position{3, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{3, 2}, Position{3, 6}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 2}, Position{4, 2}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 2}, Position{5, 2}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{2, 2}, Position{6, 2}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 5}, Position{4, 1}}, 3)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 0}, Position{5, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{2, 1}, Position{6, 5}}, 1)
+
+	assertThat(discovery, t).hasTotal(17)
+}
+
+//|x____x|
+//|ox___x|
+//|___x__|
+//|____x_|
+//|____ox|
+func TestEvaluateAIPositions5(t *testing.T) {
+	myPos := newPositionSet()
+	myPos.Add(Position{0, 0})
+	myPos.Add(Position{0, 5})
+	myPos.Add(Position{1, 1})
+	myPos.Add(Position{1, 5})
+	myPos.Add(Position{2, 3})
+	myPos.Add(Position{3, 4})
+	myPos.Add(Position{4, 5})
+
+	opponentPos := newPositionSet()
+	opponentPos.Add(Position{1, 0})
+	opponentPos.Add(Position{4, 4})
+	area := Area{5, 6}
+
+	state := gameState{
+		area:              area,
+		aiPositions:       myPos,
+		opponentPositions: opponentPos,
+	}
+
+	discovery := EvaluateAIPositions(state)
+
+	t.Logf("%v\n", discovery)
+
+	assertThat(discovery, t).hasSegment(segment{Position{0, 0}, Position{0, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{0, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{1, 1}, Position{1, 5}}, 2)
+	assertThat(discovery, t).hasSegment(segment{Position{2, 0}, Position{2, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{2, 1}, Position{2, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{3, 0}, Position{3, 4}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{3, 1}, Position{3, 5}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 5}, Position{4, 5}}, 3)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 5}, Position{4, 1}}, 2)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 3}, Position{4, 3}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{4, 1}}, 1)
+	assertThat(discovery, t).hasSegment(segment{Position{0, 1}, Position{4, 5}}, 3)
+	assertThat(discovery, t).hasTotal(12)
 }
